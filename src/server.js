5c435e69eb26b740dfd79663e84fc9df7ab02043
Yorí <yorigcsdev@gmail.com>
2022-07-22 15:02:50 -0300

    Feature: init express server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const server = express();
+server.use([cors(), express.json()]);
+
+server.listen(PORT, ()=> console.log('listening on port '+PORT));
