1ee7a61cda9ba460ede4bed5ab2d5e5316bfe212
Yorí <yorigcsdev@gmail.com>
2022-07-23 16:36:39 -0300

    Feature: add getCustomerByIdController

--- src/routes/routeCustomers.js
+++ src/routes/routeCustomers.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import getCustomerByIdController from "../controllers/getCustomerByIdController.js";
 import getCustomersController from "../controllers/getCustomersController.js";
 
 const routes = Router();
 
 routes.get('/customers',getCustomersController)
+routes.get('/customers/:id',getCustomerByIdController)
 routes.post('/customers')
 routes.put('/customers')
 
