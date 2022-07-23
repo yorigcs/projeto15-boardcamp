abb5c3b7c1312fe883e27fca6fc536eb8951c6ca
Yorí <yorigcsdev@gmail.com>
2022-07-23 15:57:40 -0300

    Feature: add validatePostGamesMiddleware

--- src/routes/routeGames.js
+++ src/routes/routeGames.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import getGamesController from "../controllers/getGamesController.js";
+import validatePostGamesMiddleware from "../middlewares/validatePostGamesMiddleware.js";
 
 
 const routes = Router();
 routes.get('/games',getGamesController)
+routes.post('/games',validatePostGamesMiddleware)
 export default routes;
--- src/schemas/gamesSchema.js
+++ src/schemas/gamesSchema.js
@@ -3,7 +3,8 @@ import Joi from 'joi';
 const gameSchema = Joi.object({
     name: Joi.string().required(),
     stockTotal: Joi.number().required().min(1),
-    categoryId: Joi.number().required().min(1),
+    pricePerDay: Joi.number().required().min(1),
+    categoryId: Joi.number().required().min(1)
 })
 
 export default gameSchema;
