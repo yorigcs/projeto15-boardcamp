9775b833d13a94d60622477e370133ce3abac859
Yorí <yorigcsdev@gmail.com>
2022-07-25 15:52:48 -0300

    Feature: add getRentalsController

--- src/routes/routeRentals.js
+++ src/routes/routeRentals.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import getRentalsController from "../controllers/getRentalsController.js";
 import postRentalsController from "../controllers/postRentalsController.js";
 import validatePostRentalsMiddleware from "../middlewares/validatePostRentalsMiddleware.js";
 
 const routes = Router();
 
-routes.post('/rentals',validatePostRentalsMiddleware, postRentalsController);
+routes.get('/rentals', getRentalsController);
+routes.post('/rentals', validatePostRentalsMiddleware, postRentalsController);
 
 export default routes;
